352ecf0060d16f01e8e0924102fd90229bda474e
Your Name <you@example.com>
2025-01-08 14:49:09 +0200

    vesion 2

---
+++ bootstrap-5.3.3-dist/js/main.js
@@ -0,0 +1,63 @@
+
+
+let cars=["TOYOTA","BENZ","MAZDA","BMW",]
+
+
+
+
+
+onst fruits = ["apple","banana","cherry"];
+for (let i = 0;i<fruits.length;i++){
+    console.log(fruits[i])
+}
+
+
+let x =1
+while(x,5){
+    console .log (x)
+    x++
+}
+
+const num= [1,2,3,4];
+let sum=0
+
+for (let i= 0 ; i < num.length; i++){
+console.log ("total", sum)
+}
+
+let numbers=[12,34,5,67,89];
+for (i=0;i< numbers.length ;i++){
+    if(numbers[i]> largest){
+        largest=numbers[i];
+    }
+}
+
+console .log(largest)c
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
